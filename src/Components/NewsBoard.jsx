14c469ab97938ee796e41ef7754ba5c97244effd
DevNitesh10 <princenitesh325@gmail.com>
2024-03-23 23:54:36 +0545

    Navbar Newsboard and NewsItems Done

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const fetchNews = async () => {
+            try {
+                const API_KEY = import.meta.env.VITE_API_KEY;
+                const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`;
+                const response = await fetch(url);
+                const data = await response.json();
+                setArticles(data.articles);
+            } catch (error) {
+                console.error('Error fetching news:', error);
+            }
+        };
+
+        fetchNews();
+    }, []);
+
+    // 
+    return (
+        <div className="container">
+            <h2 className="text-center mb-4">Latest <span className="badge bg-danger">News</span></h2>
+            <div className="row">
+                {articles.map((article, index) => (
+                    <NewsItem
+                        key={index}
+                        title={article.title}
+                        description={article.description}
+                        image={article.urlToImage}
+                        url={article.url}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+
+};
+
+export default NewsBoard;
